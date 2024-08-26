752d60ea4659e2178e1a06005ca4250678e6a0ce
grv-saini-20 <gouravsaini9797@gmail.com>
2024-08-26 21:45:50 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import './App.css'
+import  FileIcon from "./assets/images/FileIcon.svg";
+import Cancel  from "./assets/images/Cancel.svg";
+
+function App() {
+  let [files, setFiles] = useState<File[]>([]);
+
+  const handleFileInput = (e:any) => {
+    setFiles(Array.from(e.target.files));
+  }
+
+  const handleDelete = (index: number) => {
+    alert(index)
+    files = files.filter((x,i) => i !== index )
+  }
+
+  return (
+    <>
+    <div className="dropbox">
+    <label htmlFor="dropbox">
+      <img src={FileIcon} alt="file-icon" />
+    </label>
+      <h1>Images</h1>
+    <input type="file" id="dropbox" className="input-file" accept='image/*' multiple onChange={handleFileInput}/>
+      <p className='browse-text'>
+        Drag & Drop images or <label htmlFor="dropbox" className='browse'> <u> Browse </u></label>
+      </p>
+    </div>
+    {files.length !== 0 &&  (
+      <div className="all-files">
+        {files.map((file,index) => (
+        <div className="thumbnail-body">
+          <img className='thumbnail' key={index} src={URL.createObjectURL(file)} alt="uploaded-file" />
+          <img width="20px" height="20px" className='cancel' src={Cancel} alt="cancel" onClick={() => handleDelete(index)} />
+        </div>
+        ))}
+    </div>
+    )}
+    </>
+  )
+}
+
+export default App
